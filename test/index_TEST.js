dbb6b574a47f267e681afba93409b61e5723fe93
asciidisco <public@asciidisco.com>
2013-08-20 19:09:55 +0200

    Added build/release automation tools

--- test/index_TEST.js
+++ test/index_TEST.js
@@ -6,6 +6,7 @@ var Reporter = require('../index.js');
 describe('dalek-internal-reporter', function() {
 
   it('should exist', function() {
+    console.log(Reporter);
     expect(Reporter).to.be.ok;
   });
 
